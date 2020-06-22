d7a9aac27cf1e0a32cf3b470d9d4d06bb49d8c85
carigonz <carolinagonzalez794@gmail.com>
2020-06-22 15:43:00 -0300

    updated router shop view, with collection view

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   );
 }
