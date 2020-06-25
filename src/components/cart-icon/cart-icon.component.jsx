e6032b34a1b840b5507cc0a01e98beb8c23d5ad8
carigonz <carolinagonzalez794@gmail.com>
2020-06-25 20:43:28 -0300

    added selectors to cart items and cart items count for redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
-import { toogleCartHidden } from '../../redux/cart/cart.actions'
+import { toogleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartICon = ({ toogleCartHidden }) => (
+const CartICon = ({ toogleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toogleCartHidden}>
     <ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 )
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+})
 const mapDispatchToProps = dispatch => ({
   toogleCartHidden: () => dispatch(toogleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartICon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartICon);
